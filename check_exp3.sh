7c5e8ff3c3b40451b5d4fdd18fee69b1c119385d
lyhhhhhhhhhhh <58461639+lyhhhhhhhhhhh@users.noreply.github.com>
2019-12-17 14:25:16 +0800

    Update check_exp3.sh

--- check_exp3.sh
+++ check_exp3.sh
@@ -1,1 +1,18 @@
- 
+ #! /bin/bash
+
+file1=/home/lyhhhhh/study/Operating_System/exp/doc/input.txt
+file2=/home/lyhhhhh/study/Operating_System/exp/doc/output.txt
+
+diff -q $file1 $file2 
+
+if [ $? != 0 ]
+
+then
+
+    echo "Different!"
+
+else
+
+    echo "Same!"
+
+fi
